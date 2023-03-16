1a12095e4d848c2bd7da88ddab0fbe3a911b313f
loganbyeon <113130701+loganbyeon@users.noreply.github.com>
2023-03-15 22:08:48 -0700

    Delete Main.java

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-package u7pp;
-
-
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to u7pp :)\nUse this space to test your code.");
-    }
-}
